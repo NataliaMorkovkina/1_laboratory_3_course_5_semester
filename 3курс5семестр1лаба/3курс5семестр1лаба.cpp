8b3ff6944b7d2feb5d73be9bbfd9b6d05b6e5692
NataliaMorkovkina <vysotinikol@gmail.com>
2022-09-14 16:52:26 +0300

    init commit

---
+++ 3курс5семестр1лаба/3курс5семестр1лаба.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "Header.h"
+using namespace std;
+
+int main() {
+   /* switch ()
+    {
+
+    case 1:
+        ();
+        break;
+    case 2:
+        ();
+        ();
+    case 3:
+        ();
+        break;
+    default:
+        ();
+        break;
+    }
+
+    return 0;*/
+
+}
