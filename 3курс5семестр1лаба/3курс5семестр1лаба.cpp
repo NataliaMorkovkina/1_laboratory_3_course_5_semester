252785812e7b76c0033512c51eed761fe10abc26
NataliaMorkovkina <vysotinikol@gmail.com>
2022-09-15 16:42:20 +0300

    second commit

---
+++ 3курс5семестр1лаба/3курс5семестр1лаба.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include "Header.h"
+using namespace std;
+
+
+int main() {
+    int a;
+    list l1;
+    list l2;
+    cout << "Viberite deistvie" << endl;
+    cout << "1 - dobavit' element" << endl;
+    cout << "2 - ydalit' element" << endl;
+    cout << "3 - pomenyat' element" << endl;
+    cout << "4 - posmotret' informaciyu" << endl;
+    cout << "5 - zavershit' programmu" << endl;
+    cin >> a;
+    
+
+    switch (a) {
+
+    case 1:
+        int b;
+        cout << "Vi hotite" << endl;
+        cout << "1 - dobavit' v konec" << endl;
+        cout << "2 - dobavit' v nachalo" << endl;
+        cout << "3 - dobavit' po nomeru elementa" << endl;
+        cout << "4 - nazad" << endl;
+        cin >> b;
+        switch (b) {
+
+        case 1:
+            int p_b;
+            cout << "Vvedite chislo, kotoroe hotite dobavit'" << endl;
+            cin >> p_b;
+            l1.push_back(p_b);
+            break;
+        case 2:
+            int p_f;
+            cout << "Vvedite chislo, kotoroe hotite dobavit'" << endl;
+            cin >> p_f;
+            l1.push_front(p_f);
+            break;
+
+        case 3:
+            break;
+        case 4:
+            break;
+        }
+
+
+    case 2:
+        int c;
+        cout << "Vi hotite" << endl;
+        cout << "1 - ydalit' s konca" << endl;
+        cout << "2 - ydalit' is nachala" << endl;
+        cout << "3 - ydalit' po nomeru elementa" << endl;
+        cout << "4 - nazad" << endl;
+        cin >> c;
+
+        switch (c) {
+        case 1:
+            l1.remove_last();
+            cout << "chislo ydaleno!" << endl;
+
+        case 2:
+            l1.remove_first();
+            cout << "chislo ydaleno!" << endl;
+
+        case 3:
+
+         break;
+        }
+
+
+    case 3:
+        break;
+
+
+    case 4:
+        break;
+
+   
+    }
+    return 0;
+
+}
---
+++ 3курс5семестр1лаба/Header.h
@@ -0,0 +1,193 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* next;
+
+    Node(int _val) : val(_val), next(nullptr) {}
+};
+
+
+struct list {
+    Node* first;
+    Node* last;
+    Node* index;
+
+    list() : first(nullptr), last(nullptr) {}
+
+    bool is_empty() {
+        return first == nullptr;
+    }
+
+    void push_back(int _val) {
+        Node* p = new Node(_val);
+        if (is_empty()) {
+            first = p;
+            last = p;
+            return;
+        }
+        last->next = p;
+        last = p;
+    }
+
+    void push_front(int _val) {
+        Node* p = new Node(_val);
+        if (is_empty()) {
+            first = p;
+            last = p;
+            return;
+        }
+
+        p->next = first;
+        first = p;
+       
+    }
+
+    void push_by_index(int _val) {
+        Node* p = new Node(_val);
+        int count;
+        cout << "vvedite nomer elementa\t";
+        cin >> count;
+
+        if (is_empty()) {
+            first = p;
+            last = p;
+            return;
+        }
+
+        for (int i = 0; i < size(); i++) {
+            if (i == count){
+                p->next = index;
+                first = p;
+            }
+        }
+        
+    }
+
+
+    void print() {
+        if (is_empty()) return;
+        Node* p = first;
+        while (p) {
+            cout << p->val << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+
+    Node* find(int _val) {
+        Node* p = first;
+        while (p && p->val != _val) p = p->next;
+        return (p && p->val == _val) ? p : nullptr;
+    }
+
+    void remove_first() {
+        if (is_empty()) return;
+        Node* p = first;
+        first = p->next;
+        delete p;
+    }
+
+    void remove_last() {
+        if (is_empty()) return;
+        if (first == last) {
+            remove_first();
+            return;
+        }
+        Node* p = first;
+        while (p->next != last) p = p->next;
+        p->next = nullptr;
+        delete last;
+        last = p;
+    }
+
+    void remove_by_index() {
+        if (is_empty()) return;
+        if (first == last) {
+            remove_first();
+            return;
+        }
+        for (int i = 0; i < size(); i++) {
+
+        }
+    }
+
+
+    void remove(int _val) {
+        if (is_empty()) return;
+        if (first->val == _val) {
+            remove_first();
+            return;
+        }
+        else if (last->val == _val) {
+            remove_last();
+            return;
+        }
+        Node* slow = first;
+        Node* fast = first->next;
+        while (fast && fast->val != _val) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if (!fast) {
+            cout << "This element does not exist" << endl;
+            return;
+        }
+        slow->next = fast->next;
+        delete fast;
+    }
+
+    Node* operator[] (const int index) {
+        if (is_empty()) return nullptr;
+        Node* p = first;
+        for (int i = 0; i < index; i++) {
+            p = p->next;
+            if (!p) return nullptr;
+        }
+        return p;
+    }
+
+
+    int size() {
+        if (is_empty()) {
+            return 0;
+        }
+        int n = 1;
+        Node* p = first;
+        while (p != last) {
+            n++;
+            p = p->next;
+        }
+        return n;
+    }
+
+    bool is_list_existing(list other){
+        Node* p = first;
+        bool ok = false;
+        for (int i = 0; i < size() - other.size() + 1; i++){
+            Node* p1 = p;
+            Node* p2 = other.first;
+            bool ok1 = true;
+            for (int j = 0; j < other.size(); j++){
+                if (p1->val == p2->val)
+                {
+                    p1 = p1->next;
+                    p2 = p2->next;
+                }
+                else
+                {
+                    ok1 = false;
+                    break;
+                }
+            }
+            if (ok1)
+            {
+                ok = true;
+                break;
+            }
+            p = p->next;
+        }
+        return ok;
+    }
+};
